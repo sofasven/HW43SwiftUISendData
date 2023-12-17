c10c685f3a50dbc407a11777fcdf73873d272207
Sofa <sofasven00@gmail.com>
2023-12-17 18:42:20 +0300

    Project created

---
+++ SwiftUISendData/SwiftUISendData/SwiftUISendDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISendDataApp.swift
+//  SwiftUISendData
+//
+//  Created by Sofa on 17.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISendDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
